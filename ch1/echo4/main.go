94cfd715ecd3b5e254b9fabe74e040e80257fac0
Andy Kitson <andrew.kitson@gmail.com>
2017-06-13 19:49:48 -0500

    ex 1.3: performance of joining vs. concatenating

--- ch1/echo4/main.go
+++ ch1/echo4/main.go
@@ -3,10 +3,31 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
-	for ix, arg := range os.Args[1:] {
-		fmt.Println(ix, arg)
+	iterations := 10 * 1000 * 1000
+	sep := " "
+	args := ""
+
+	// test joining
+	start := time.Now()
+	for i := 0; i < iterations; i++ {
+		args = strings.Join(os.Args[1:], sep)
 	}
+	end := time.Since(start).Seconds()
+	fmt.Printf("Joining took %.2fs seconds. ", end)
+
+	// test concatenating
+	start = time.Now()
+	for i := 0; i < iterations; i++ {
+		args = ""
+		for _, arg := range os.Args[1:] {
+			args += sep + arg
+		}
+	}
+	end = time.Since(start).Seconds()
+	fmt.Printf("Concatenating took %.2fs seconds. ", end)
 }
